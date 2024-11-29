1feb57b227c566573e5ae492c6bf7c510c32758a
CLINTON-VM <74558654@idexperujapon.edu.pe>
2024-11-29 16:30:56 -0500

    primera practica

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\controller74558654;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+route::get('/74558654',[Controller74558654::class,'index']);
