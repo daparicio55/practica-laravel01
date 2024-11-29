2783202b8def477313fcdc62245c309746c61b82
AGUIL710 <aguilbertoreynamori@gmail.com>
2024-11-29 16:29:44 -0500

    primera practica

---
+++ app/Http/Controllers/Controller72096593.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Controller72096593 extends Controller
+{
+    //
+    public function index()
+    {
+        return view('72096593.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\Controller72096593;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\Routing\Route as RoutingRoute;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('72096593/',[Controller72096593::class,'index'])->name('72096593.index');
