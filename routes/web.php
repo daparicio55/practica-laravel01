456111f6d40e2aa5a991aa36424435195d871108
Sayco25 <75623496@idexperujapon.edu.pe>
2024-11-29 16:30:59 -0500

    primer commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Controller75623496;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+route::get('75623496',[Controller76731601::class, 'index']);
