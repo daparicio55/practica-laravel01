a51ae702bf44e9cb784842cd1a0dd9d39e2a89ff
Dayanita196 <dayanahuaman347@gmail.com>
2024-11-29 16:30:50 -0500

    primer commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\Controller72807549;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/72807549', [Controller72807549::class,'index']);
