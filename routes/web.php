27c1de8dcafce23bb398a8118d58ee97f68ca51c
eduarcc99 <eduarcastilloculquerricra@gmail.com>
2024-11-29 22:38:59 +0100

    primer commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\Controller75926437;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/75926437', [Controller75926437::class, 'index']);
