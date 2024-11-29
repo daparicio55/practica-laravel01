d0fe3e7e06a60592d8fcccf55e06bdf1f983798d
daparicio85 <43053643@idexperujapon.edu.pe>
2024-11-29 16:30:14 -0500

    primer commit

---
+++ app/Http/Controllers/Controller43053643.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Controller43053643 extends Controller
+{
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Controller43053643;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/43053643',[Controller43053643::class,'index']);
