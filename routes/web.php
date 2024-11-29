b4a162fdc30e60c7e38693fd9bf3712ae5c9f48d
unknown <noylyguzmanalva@gmail.com>
2024-11-29 16:29:38 -0500

    primer commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
-
+use app\Http\Controllers\Controller76731601;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+ route::get('76731601',[Controller76731601::class, 'index']);
+
+ 
