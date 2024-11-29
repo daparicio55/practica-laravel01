f00dd20a0ecf7bc95e2f06a8037948a12b7ba5e7
atlas4022j <atlas4022j@gmail.com>
2024-11-29 16:31:13 -0500

    primer commit

---
+++ app/Http/Controllers/Controller72174435.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Libro;
+
+use Illuminate\Http\Request;
+
+class Controller72174435 extends Controller
+{
+ //
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller72174435;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/72174435', [Controller72174435::class, 'index']);
+
