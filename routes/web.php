68edd213315de95725b62d3093f7085c1a7ff622
Deysicorrales06 <76816143@idexperujapon.edu.pe>
2024-11-29 17:57:11 -0500

    ahora si lo hize bien

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
-
+use App\Http\Controllers\Controller76816143;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/76816143',[Controller76816143::class]);
