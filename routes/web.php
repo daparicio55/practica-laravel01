d2b873e0985513fb7e21144c38d78e2d5c4502df
sandraVH455 <sandravillarhuman@gmail.com>
2024-11-29 16:52:25 -0500

    primer commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\controller73832729;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+route::get('/73832729',[controller73832729::class,'index']);
