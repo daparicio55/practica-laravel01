ce1beb3129cca91befe6c1785633b57760445a19
Dayane <nikolpuscan@gmail.com>
2024-11-29 16:33:35 -0500

    mi primer commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller72732505;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get ('/72732505', [Controller72732505::class,'index']); 
