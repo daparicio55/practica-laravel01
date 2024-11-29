c5a34959c9a54b8c14c1a393bfdfb8f9f008ce72
sifuentesgemner <sifuentesgemner@gmail.comm>
2024-11-29 16:32:33 -0500

    canbios en el archivo web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Controller74566493;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+route::get('/74566493',[Controller74566493::class,'index']);
